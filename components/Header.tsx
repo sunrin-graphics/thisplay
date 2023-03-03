e0c559b605a25a684d2f6a22657ddcf91fcdd498
wugusaud77 <wugusaud77@icloud.com>
2023-03-03 19:30:25 +0900

    tlqkf

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,87 +4,82 @@ import Logo from "../assets/logo.svg";
 import LogoActive from "../assets/logoActive.svg";
 
 const Layout = styled.header`
-    width:100vw;
-    height: 60px;
-    background: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
+  width: 100vw;
+  height: 60px;
+  background: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  z-index: 99;
 `;
 
 const Box = styled.div`
-    width: 1440px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  width: 1440px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const List = styled.ul`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  gap:24px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+`;
 
 const ClubSelectButton = styled.button`
-    height: 40px;
-    background: none;
-  border:none;
+  height: 40px;
+  background: none;
+  border: none;
   font-weight: 500;
   font-size: 16px;
   line-height: 150%;
   display: flex;
   align-items: center;
   text-align: center;
-  gap:2px;
-  color: rgba(51, 32, 24, 0.65);
+  gap: 2px;
+  //color: rgba(51, 32, 24, 0.65);
+  color: #ffffff;
 `;
 
 const Icon = styled.div`
   font-size: 24px;
-  color: #7A6E69;
-`
-
-const LinkStyled = styled.a`
-font-weight: 500;
-font-size: 16px;
-line-height: 150%;
-/* identical to box height, or 24px */
-
-display: flex;
-align-items: center;
-text-align: center;
-
-color: rgba(51, 32, 24, 0.65);
+  //color: #7a6e69;
+  color: #ffffff;
 `;
 
+const LinkStyled = styled.a`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 150%;
+  /* identical to box height, or 24px */
 
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  //color: rgba(51, 32, 24, 0.65);
+  color: #ffffff;
+`;
 
 const Header = () => {
-    return (
-        <Layout>
-            <Box>
-                <Image
-                    src={LogoActive}
-                    alt="Logo"
-                    width={100}
-                    height={100}
-                />
-                <List>
-                    <LinkStyled>시연회 소개</LinkStyled>
-                    <LinkStyled>동아리 목록</LinkStyled>
-                    <LinkStyled>질문과 답변</LinkStyled>
-                </List>
-                <ClubSelectButton>
-                    동아리 소개
-                    <Icon className="material-symbols-rounded">
-                        expand_more
-                    </Icon>
-                </ClubSelectButton>
-            </Box>
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <Box>
+        <Image src={Logo} alt="Logo" width={77} height={20} />
+        <List>
+          <LinkStyled>시연회 소개</LinkStyled>
+          <LinkStyled>동아리 목록</LinkStyled>
+          <LinkStyled>질문과 답변</LinkStyled>
+        </List>
+        <ClubSelectButton>
+          동아리 소개
+          <Icon className="material-symbols-rounded">expand_more</Icon>
+        </ClubSelectButton>
+      </Box>
+    </Layout>
+  );
+};
 
 export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(mp4|ogg)$/, // 로더를 적용할 파일 유형
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        // 해당 파일에 적용할 로더의 이름
+        loader: "file-loader",
+        options: {
+          name: "assets/media/[name].[ext]?[hash]",
+        },
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
