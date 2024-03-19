f2fa4c49eaca47553a0ee1428321861ba262c779
Doyun Yu <doyun0109@icloud.com>
2024-03-19 10:22:02 +0900

    Chore: edit nextjs config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(mp4|ogg)$/, // 로더를 적용할 파일 유형
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        // 해당 파일에 적용할 로더의 이름
-        loader: "file-loader",
-        options: {
-          name: "assets/media/[name].[ext]?[hash]",
-        },
-      },
-    });
-    return config;
-  },
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(mp4|ogg)$/, // 로더를 적용할 파일 유형
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                // 해당 파일에 적용할 로더의 이름
+                loader: "file-loader",
+                options: {
+                    name: "assets/media/[name].[ext]?[hash]",
+                },
+            },
+        });
+        return config;
+    },
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
